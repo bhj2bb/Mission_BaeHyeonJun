3e24611963aefd2dc67bc50125c61138a1b88b50
bhj2bb <cs2uee@gmail.com>
2023-11-06 11:07:38 +0900

    5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -7,6 +9,8 @@ public class App {
         System.out.println("====== 명언 앱 ======");
 
         int lastTextId = 0;
+        List<Text> texts = new ArrayList<>();
+
 
         while (true) {
             System.out.print("명령) ");
@@ -26,13 +30,32 @@ public class App {
                 String authorName = scanner.nextLine();
 
                 lastTextId++;
+                int id = lastTextId;
+                Text text = new Text(id, content, authorName);
+                texts.add(text);
 
                 System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastTextId);
+                System.out.printf("%d번 명언이 등록되었습니다.\n\n", lastTextId);
 
             }
+            else if (cmd.equals("목록")) {
+                System.out.println("총 개수 : " + texts.size());
+
+
+                System.out.println("번호 | 작가 | 명언");
+                System.out.println("----------------------");
+
+                if (texts.isEmpty())
+                    System.out.println("등록된 명언이 없습니다.\n");
+
+                for (int i = texts.size() - 1; i >= 0; i--) {
+                    Text text = texts.get(i);
+                    System.out.printf("%d / %s / %s\n\n", text.id, text.authorName, text.content);
+                }
+            }
+
             else{
-                System.out.println("잘못된 명령입니다. 다시 입력하시오.");
+                System.out.println("잘못된 명령입니다. 다시 입력하시오.\n");
             }
 
         }
